e3d62137e020370d2629d3b62c386f1f532d2c6d
Coyote <Coyote@gmail.com>
2024-05-01 23:41:28 +0300

    Initial Commit

---
+++ NewApp_3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewApp_3
+//
+//  Created by SERGEI OSKIN on 01.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
